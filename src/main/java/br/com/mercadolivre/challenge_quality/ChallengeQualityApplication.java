593298b1f91c18782642798a38a414eca85a4d3a
israelmercadolivre <israel.melo@mercadolivre.com>
2021-06-10 13:05:43 -0300

    Init project

---
+++ src/main/java/br/com/mercadolivre/challenge_quality/ChallengeQualityApplication.java
@@ -0,0 +1,13 @@
+package br.com.mercadolivre.challenge_quality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeQualityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeQualityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/mercadolivre/challenge_quality/ChallengeQualityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mercadolivre.challenge_quality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeQualityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
