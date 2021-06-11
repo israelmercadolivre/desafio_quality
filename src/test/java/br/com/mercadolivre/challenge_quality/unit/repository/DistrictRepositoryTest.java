703aec71d6335646367cb1dd067b8998cdc24bb5
israelmercadolivre <israel.melo@mercadolivre.com>
2021-06-11 12:31:12 -0300

    Added unit test to Repository and Service

---
+++ src/test/java/br/com/mercadolivre/challenge_quality/unit/repository/DistrictRepositoryTest.java
@@ -0,0 +1,44 @@
+package br.com.mercadolivre.challenge_quality.unit.repository;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Map;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DistrictRepositoryTest {
+
+    private static DistrictRepository districtRepository;
+
+    @BeforeAll
+    public static void setUp(){
+        districtRepository = new DistrictRepository();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Vila Olimpia", "belem", "ALPHAVILLE"})
+    public void shouldFindDistrictWithNamesValid(String name){
+        Optional<Map.Entry<String, Double>> district = districtRepository.findByName(name);
+        assertNotNull(district);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Belim", "Diadema", "Santo Andre"})
+    public void shouldNotFindDistrictWithNamesInvalid(String name){
+        Optional<Map.Entry<String, Double>> district = districtRepository.findByName(name);
+        assertTrue(district.isEmpty());
+
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    public void shouldNotFindDistrictWithValueNullAndEmpty(String name){
+        Optional<Map.Entry<String, Double>> district = districtRepository.findByName(name);
+        assertTrue(district.isEmpty());
+
+    }
+}
---
+++ src/test/java/br/com/mercadolivre/challenge_quality/unit/service/PropertyServiceTest.java
@@ -0,0 +1,125 @@
+package br.com.mercadolivre.challenge_quality.unit.service;
+
+import br.com.mercadolivre.challenge_quality.dto.PropertyDto;
+import br.com.mercadolivre.challenge_quality.dto.RoomDto;
+import br.com.mercadolivre.challenge_quality.dto.TotSquareMeter;
+import br.com.mercadolivre.challenge_quality.exception.DistrictNotFoundException;
+import br.com.mercadolivre.challenge_quality.unit.repository.DistrictRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.mock;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+class PropertyServiceTest {
+
+    private static final String SALA = "Sala";
+    private static final String COZINHA = "Cozinha";
+    private static final String BANHEIRO = "Banheiro";
+    private static final double SALA_LENGTH = 20d;
+    private static final double SALA_WIDTH = 11d;
+    private static final double COZINHA_LENGTH = 15d;
+    private static final double COZINHA_WIDTH = 11d;
+    private static final double BANHEIRO_LENGTH = 10d;
+    private static final double BANHEIRO_WIDTH = 6d;
+    private static PropertyService service;
+    @Mock
+    private static DistrictRepository repository;
+
+    private static PropertyDto dto;
+
+    @BeforeAll
+    public static void setUp(){
+        dto = generatePropertyDtoValid();
+        repository = mock(DistrictRepository.class);
+        service = new PropertyService(repository);
+    }
+
+    @BeforeEach
+    public void mockRepository(){
+        Map.Entry<String, Double> district = mock(Map.Entry.class);
+        when(district.getValue()).thenReturn(700d);
+        Mockito.when(repository.findByName(dto.getProp_district())).thenReturn(Optional.of(district));
+    }
+
+    @Test
+    public void shouldNotCalculateSquareMeterWithDistrictInvalid(){
+        PropertyDto dto = new PropertyDto();
+        String districtInvalid = "Belim";
+        dto.setProp_district(districtInvalid);
+        Mockito.when(repository.findByName(districtInvalid)).thenReturn(Optional.empty());
+
+        Exception exception = assertThrows(DistrictNotFoundException.class, () -> {
+            service.calculateSquareMeter(dto);
+        });
+
+        String expectedMessage = "Not found district ["+districtInvalid+"]";
+        String actualMessage = exception.getMessage();
+
+        assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    public void shouldCalculateSquareMeterCorrectly(){
+        TotSquareMeter result = service.calculateSquareMeter(dto);
+        assertEquals(dto.getProp_name(), result.getName());
+        assertEquals(445d, result.getTotSquareMeter());
+    }
+
+    @Test
+    public void shouldCalculateValuePropertyCorrectly(){
+
+        TotSquareMeter result = service.calculateValueProperty(dto);
+        assertEquals(dto.getProp_name(), result.getName());
+        assertEquals(311500d, result.getValorTotal().doubleValue());
+    }
+
+    @Test
+    public void shouldReturnBiggerRoomCorrectly(){
+        RoomDto result = service.biggerRoom(dto);
+        assertEquals("Sala", result.getRoom_name());
+        assertEquals(11, result.getRoom_width());
+        assertEquals(20, result.getRoom_length());
+    }
+
+    @Test
+    public void shouldCalculateSquareMeterRoomCorrectly(){
+        List<RoomDto> result = service.calculateSquareMeterRoom(dto);
+        assertEquals(SALA, result.get(0).getRoom_name());
+        assertEquals(SALA_WIDTH, result.get(0).getRoom_width());
+        assertEquals(SALA_LENGTH, result.get(0).getRoom_length());
+        assertEquals(220, result.get(0).getSquareMeter());
+
+        assertEquals(COZINHA, result.get(1).getRoom_name());
+        assertEquals(COZINHA_WIDTH, result.get(1).getRoom_width());
+        assertEquals(COZINHA_LENGTH, result.get(1).getRoom_length());
+        assertEquals(165, result.get(1).getSquareMeter());
+
+        assertEquals(BANHEIRO, result.get(2).getRoom_name());
+        assertEquals(BANHEIRO_WIDTH, result.get(2).getRoom_width());
+        assertEquals(BANHEIRO_LENGTH, result.get(2).getRoom_length());
+        assertEquals(60, result.get(2).getSquareMeter());
+    }
+
+    private static PropertyDto generatePropertyDtoValid(){
+        PropertyDto dto = new PropertyDto();
+        dto.setProp_name("Casa Teste");
+        dto.setProp_district("VILA OLIMPIA");
+
+        RoomDto roomDto1 = new RoomDto(SALA, SALA_LENGTH, SALA_WIDTH);
+        RoomDto roomDto2 = new RoomDto(COZINHA, COZINHA_LENGTH, COZINHA_WIDTH);
+        RoomDto roomDto3 = new RoomDto(BANHEIRO, BANHEIRO_LENGTH, BANHEIRO_WIDTH);
+        dto.setRooms(Arrays.asList(roomDto1, roomDto2, roomDto3));
+
+        return dto;
+    }
+}
